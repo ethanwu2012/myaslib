bae6aa791483370576ee27e544397ec55abdc456
ethanwu2012 <ethan_wu2012@163.com>
2016-07-05 14:31:09 +0800

    first commit

---
+++ app/src/main/java/com/zyt/myaslib/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zyt.myaslib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zyt.mylib.MyUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyUtil.getData();
+    }
+}
---
+++ mylib/src/main/java/com/zyt/mylib/MyUtil.java
@@ -0,0 +1,11 @@
+package com.zyt.mylib;
+
+/**
+ * Created by wuzz on 2016/7/5.
+ */
+public class MyUtil {
+
+    public static String getData() {
+        return "data";
+    }
+}
